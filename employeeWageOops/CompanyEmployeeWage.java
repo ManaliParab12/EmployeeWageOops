255b9fae890c91c3855562dacc8d9674e496fffc
ManaliParab12 <manaliparab10@gmail.com>
2020-11-02 14:50:46 +0530

    Company Employee wage pojo class created with Attributes and ArrayList

---
+++ employeeWageOops/CompanyEmployeeWage.java
@@ -0,0 +1,14 @@
+package employeeWageOops;
+
+import java.util.ArrayList;
+
+class CompanyEmployeeWage {
+
+public final String company;
+public final int empRatePerHr;
+public final int numberOfWorkingDays;
+public final int maxHrPerMonth;
+public int totalEmpWage;
+ArrayList<Integer> dailyWage = new ArrayList<Integer>();
+
+}
