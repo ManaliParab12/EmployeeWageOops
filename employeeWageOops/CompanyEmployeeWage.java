66b87baa8e2ef885f3052cc0447ad9af3d0cb9d8
ManaliParab12 <manaliparab10@gmail.com>
2020-11-02 14:55:24 +0530

    Constructor, setter and toString method created in Employee wage pojo class

--- employeeWageOops/CompanyEmployeeWage.java
+++ employeeWageOops/CompanyEmployeeWage.java
@@ -11,4 +11,20 @@ public final int maxHrPerMonth;
 public int totalEmpWage;
 ArrayList<Integer> dailyWage = new ArrayList<Integer>();
 
+public CompanyEmpWage(String company, int empRatePerHr, int numberOfWorkingDays, int maxHrPerMonth) {
+	  this.company = company;
+	  this.empRatePerHr = empRatePerHr;
+	  this.numberOfWorkingDays = numberOfWorkingDays;
+	  this.maxHrPerMonth = maxHrPerMonth;
+	}
+
+	public void setTotalEmpWage(int totalEmpWage) {
+	  this.totalEmpWage = totalEmpWage;
+	}
+
+	public String toString() {
+	  return "Total employee wages for: " + company + " is " + totalEmpWage;
+	}
+
+
 }
